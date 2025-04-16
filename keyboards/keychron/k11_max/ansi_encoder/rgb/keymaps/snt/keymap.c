1578ff0ade19cfb165156f6c1f1c65af5d717783
Kentaro Shinto <shintok@gmail.com>
2025-04-16 18:42:52 +0900

    fixed: win_vim: navigation back and forward is reversed.

--- keyboards/keychron/k11_max/ansi_encoder/rgb/keymaps/snt/keymap.c
+++ keyboards/keychron/k11_max/ansi_encoder/rgb/keymaps/snt/keymap.c
@@ -38,18 +38,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_A,/**/	KC_S,	 KC_D,	  KC_F,    KC_G,    /******/  KC_H,    KC_J,	KC_K,	KC_L,	 LT(MAC_VIM,KC_SCLN),
         /**/                                                                                                           KC_QUOT,             KC_ENT,  /******/ KC_HOME,
         KC_LSFT, KC_Z,/**/  KC_X,    KC_C,    KC_V,	   KC_B,/**/KC_B,     KC_N,	   KC_M, KC_COMM, KC_DOT,/**/KC_SLSH,  KC_RSFT, /**/         KC_UP,  /******/
-        KC_ESC, KC_LOPTN, LT(MAC_FN1,KC_LANGUAGE_2),
-                                     /******/ KC_SPC, /******/ MT(MOD_LGUI,KC_SPC),LT(FN2,KC_LANGUAGE_1),/******/ KC_SPC,  /******/ MT(MOD_RGUI,KC_LANGUAGE_1),
-                                                                                                                         /******/  KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_ESC, KC_LOPTN,/**/ MT(MOD_LGUI,KC_LANGUAGE_2),KC_SPC,MT(MOD_LGUI,KC_SPC),/**/ MO(FN2), KC_LANGUAGE_1, MO(MAC_FN1), /**/ KC_LEFT, KC_DOWN, KC_RGHT),
 
     [WIN_BASE] = LAYOUT_69_ansi(
-        KC_GRV,  KC_1,	   KC_2,	 KC_3,	  KC_4,    KC_5,	KC_6,	  KC_7,    KC_8,	KC_9,	 KC_0,	   KC_MINS,  KC_EQL,   KC_BSPC, /******/ KC_MUTE,
-        KC_TAB,  KC_Q,	   KC_W,	 KC_E,	  KC_R,    KC_T,	KC_Y,	  KC_U,    KC_I,	KC_O,	 KC_P,	   KC_LBRC,  KC_RBRC,  KC_BSLS, /******/ KC_DEL,
-        MO(COMPAT),KC_A,   KC_S,	 KC_D,	  KC_F,    KC_G,    /******/  KC_H,    KC_J,	KC_K,	 KC_L,	   LT(WIN_VIM,KC_SCLN),
-                                                                                                                         KC_QUOT,  KC_ENT,  /******/ KC_HOME,
+        KC_GRV,  KC_1,	   KC_2,	 KC_3,	  KC_4,    KC_5,	KC_6,	  /******/ KC_7,    KC_8,	KC_9,	 KC_0,	   KC_MINS,  KC_EQL,   KC_BSPC, /******/ KC_MUTE,
+        KC_TAB,  KC_Q,	   KC_W,	 KC_E,	  KC_R,    KC_T,	/******/  KC_Y,	   KC_U,    KC_I,	KC_O,	 KC_P,	   KC_LBRC,  KC_RBRC,  KC_BSLS, /******/ KC_DEL,
+        MO(COMPAT),KC_A,   KC_S,	 KC_D,	  KC_F,    KC_G,    /******/  KC_H,    KC_J,	KC_K,	 KC_L,	 LT(WIN_VIM,KC_SCLN),
+                                                                                                                         KC_QUOT,           KC_ENT,  /******/ KC_HOME,
         KC_LSFT, /******/  KC_Z,	 KC_X,	  KC_C,    KC_V,	KC_B,	  KC_B,    KC_N,	KC_M,	 KC_COMM,  KC_DOT,	 KC_SLSH,  KC_RSFT, KC_UP,   /******/
-        KC_ESC , KC_LALT,  LT(WIN_FN1,KC_LANGUAGE_2),
-                                     /******/ KC_SPC,  /******/ MT(MOD_LCTL,KC_SPC),LT(FN2,KC_LANGUAGE_1), /******/ KC_SPC,/******/   MT(MOD_RCTL,KC_LANGUAGE_1), /******/  KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_ESC , KC_LALT,/**/ MT(MOD_LGUI,KC_LANGUAGE_2),KC_SPC, MT(MOD_LCTL,KC_SPC),/**/ MO(FN2), KC_LANGUAGE_1,MO(WIN_FN1), /**/ KC_LEFT, KC_DOWN, KC_RGHT),
 
     [MAC_FN1] = LAYOUT_69_ansi(
         _______, KC_BRID,  KC_BRIU, KC_MCTRL, KC_LNPAD,RGB_VAD, RGB_VAI,  KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE,  KC_VOLD,  KC_VOLU,  _______, /******/ RGB_TOG,
@@ -63,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, BT_HST1,  BT_HST2,  BT_HST3, P2P4G,   _______, _______,  _______, _______, _______, _______,  _______,	 _______,  _______, /******/ KC_INS,
         RGB_TOG, RGB_MOD,  RGB_VAI,  RGB_HUI, RGB_SAI, RGB_SPI, /******/  _______, _______, _______, _______,  _______,  _______,  _______, /******/ KC_END,
         _______, /******/  RGB_RMOD, RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD,  _______, NK_TOGG, _______, _______,  _______,  _______,  _______, KC_PGUP, /******/
-        _______, _______,  _______,  /******/ _______, /******/ _______,  _______, /******/ _______, /******/  _______,  /******/  _______, KC_PGDN, _______),
+        _______, _______,  KC_LEFT_GUI,  /******/ _______, /******/ _______,  _______, /******/ _______, /******/  _______,  /******/  _______, KC_PGDN, _______),
 
     [FN2] = LAYOUT_69_ansi(
         KC_TILD, KC_F1,    KC_F2,	 KC_F3,   KC_F4,   KC_F5,	KC_F6,	    KC_F7,   KC_F8,	  KC_F9,  KC_F10,   KC_F11,	 KC_F12,   _______, /******/ _______,
@@ -94,8 +91,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                        _______, /******/  KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT,  _______,  _______,  _______, /******/ _______,
         _______, /******/KC_MS_WH_DOWN,
                                      _______, _______, _______, _______,  _______, _______, _______, _______,  _______,  _______,  _______, _______, /******/
-        _______, _______,KC_MS_BTN2,/******/KC_MS_BTN1,/******/ _______,RALT(KC_RIGHT), /******/
-                                                                                      RALT(KC_LEFT), /******/  _______,  /******/  _______, _______, KC_SYSTEM_SLEEP),
+        _______, _______,KC_MS_BTN2,/******/KC_MS_BTN1,/******/ _______,RALT(KC_LEFT), /******/
+                                                                                      RALT(KC_RIGHT), /******/  _______,  /******/  _______, _______, KC_SYSTEM_SLEEP),
 
 
     [COMPAT] = LAYOUT_69_ansi(
